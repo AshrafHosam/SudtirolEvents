e14be0268bb9a9fd177b651d5c805a33f28f2fc5
Antoine Lequeux <antoine.lequeux.pro@gmail.com>
2026-01-31 17:02:51 +0100

    fixed port error

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
-  // Use relative URL when running with proxy, or full URL when proxy is not used
-  // If you're using 'npm start' with proxy.conf.json, change to '/api'
-  // If running Angular separately, use 'https://localhost:7088/api'
-  apiUrl: 'http://localhost:7088/api'
+  // Tthe proxy forwards requests to the backend at http://localhost:5213
+  apiUrl: '/api'
 };
